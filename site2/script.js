60e90cc923e98f9241f490465883e82159398d05
shoreline49@gmail.com <shoreline49@gmail.com>
2016-02-28 14:56:55 -0500

    styling & javascript to site2

---
+++ site2/script.js
@@ -0,0 +1,44 @@
+//alert("thanks for visiting site");
+
+//var kittens;
+//var kittens = 5;
+window.onLoad = function(e){
+
+function turtleFact() {
+	alert("some turtles can live longer than humans");
+}
+
+//turtleFact();
+
+function temptGang(gangName) {
+	alert(gangName + " Come out and play!")
+	// body...
+}
+
+function addNumbers (num1, num2) {
+	alert(num1=num2)
+	// body...
+}
+
+//addNumbers(10, 5);
+
+function square (number) {
+	return number * number;
+	// body...
+}
+
+function displaySquare (squaredNumber) {
+	console.log("your number is" + squaredNumber);
+	// body...
+}
+
+//displaySquare(square(3));
+
+var pageNode = document.getElementsByTagName('body')[0];
+pageNode.innerHTML = '<h1>Oh Noes!</h1> <p>I just changed the whole page!</p>'
+
+var title = document.getElementById('open-title');
+title.innerHTML = "Thanks for attending class!";
+
+}
+
